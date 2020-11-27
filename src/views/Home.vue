9a9a54ea243f95b02e5b49e7dcbca2eeef55f816
unknown <nunyaklah@gmail.com>
2020-11-27 18:19:04 +0000

    initial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home font-body text-gray-800">
+    <nav class="grid grid-cols-3 p-10">
+      <div class="flex">
+        <span
+          ><svg
+            class="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            ></path></svg
+        ></span>
+        <span class="ml-3 font-bold">nunyaklah@gmail.com</span>
+      </div>
+      <div></div>
+      <div>
+        <ul class="grid md:grid-cols-4">
+          <li>
+            <a href="#">Works</a>
+          </li>
+          <li>
+            <a href="#">Resume</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="flex justify-center">
+      <div class="bg-blue-100 rounded-full w-40">
+        <img src="../assets/images/nunya.png" alt="Nunya" class="w-40" />
+      </div>
+    </div>
+    <div class="flex justify-center pt-4">
+      <p class="font-bold text-2xl">Hi, I'm Nunya Klah ✌</p>
+    </div>
+    <div
+      class="flex justify-center pt-4 text-center font-bold font-head text-6xl text-gray-800"
+    >
+      <p class="font-bold">
+        Software Engineer <br />
+        Building web and mobile products, <br />
+        brands and experience
+      </p>
+    </div>
+    <div class="flex justify-center pt-8 text-center">
+      <p class="font-bold text-xl">
+        I speacialize in Front End Development (VueJs and ReactJs), <br />
+        converting UI designs into code. <br />Also count me in Mobile
+        Application Develpment <br />(IOS and Android) using Flutter😉
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
