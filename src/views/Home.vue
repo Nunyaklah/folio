da9ed20a2c1157fde8371db6710c31f729d130af
unknown <nunyaklah@gmail.com>
2020-11-29 00:15:03 +0000

    structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -46,11 +46,12 @@
       <p class="font-bold text-2xl">Hi, I'm Nunya Klah ✌</p>
     </div>
     <div
-      class="flex justify-center pt-4 text-center font-bold font-head text-6xl text-gray-800"
+      class="flex justify-center pt-4 text-center font-bold font-body text-6xl text-gray-800"
     >
-      <p class="font-bold">
+      <p class="font-bold tracking-wide">
         Software Engineer <br />
-        Building web <br> and mobile products, <br />
+        Building web <br />
+        and mobile products, <br />
         brands and experience
       </p>
     </div>
@@ -60,6 +61,339 @@
         converting UI designs into code. <br />Also count me in Mobile
         Application Develpment <br />(IOS and Android) using Flutter😉
       </p>
+    </div>
+    <div class="flex justify-center mt-10">
+      <div
+        class="bg-gray-800 text-white btn hover:shadow-inner transform hover:scale-125 transition ease-out duration-300"
+      >
+        Connect With Me
+      </div>
+    </div>
+    <div class="grid grid-cols-3 mt-20 gap-10 p-10">
+      <div class="col-span-1">
+        <span>
+          <img
+            class="mb-10 h-96 object-cover"
+            src="../assets/images/camera.jpg"
+            alt="camera"
+          />
+        </span>
+        <span>
+          <img
+            class="h-70 object-cover"
+            src="../assets/images/coffee.jpg"
+            alt="camera"
+          />
+        </span>
+      </div>
+      <div class="col-span-1">
+        <span>
+          <img
+            class="mb-10 h-96 object-cover"
+            src="../assets/images/paint.jpg"
+            alt="camera"
+          />
+        </span>
+        <span>
+          <img
+            class="h-80 object-cover"
+            src="../assets/images/home.jpg"
+            alt="camera"
+          />
+        </span>
+      </div>
+      <div class="col-span-1">
+        <span>
+          <img class="mb-10" src="../assets/images/laptop.jpg" alt="camera" />
+        </span>
+        <span>
+          <img
+            class="h-96 object-cover"
+            src="../assets/images/monitor.jpg"
+            alt="camera"
+          />
+        </span>
+      </div>
+    </div>
+    <div class="mt-20 grid grid-cols-3 p-20 gap-20">
+      <div class="">
+        <h4 class="text-3xl pb-3 border-b border-gray-700 font-bold">
+          Tech Stack
+        </h4>
+        <ul class="mt-7 text-xl">
+          <li>JavaScript • Vue</li>
+          <li>JavaScript • React</li>
+          <li>Flutter • Dart</li>
+          <li>Tailwind CSS • BootStrap • Bulma</li>
+        </ul>
+      </div>
+      <div class="">
+        <h4 class="text-3xl pb-3 border-b border-gray-700 font-bold">
+          UI Design
+        </h4>
+        <ul class="mt-7 text-xl">
+          <li>Figma</li>
+          <li>Adobe XD</li>
+          <li>Adobe Illustrator</li>
+        </ul>
+      </div>
+      <div class="">
+        <h4 class="text-3xl pb-3 border-b border-gray-700 font-bold">
+          Infrastructure
+        </h4>
+        <ul class="mt-7 text-xl">
+          <li>AWS</li>
+          <li>Google Cloud</li>
+          <li>Heroku</li>
+        </ul>
+      </div>
+    </div>
+    <div class="px-16">
+      <h4 class="font-bold text-7xl border-b border-gray-900 pb-4">
+        Who I've worked with
+      </h4>
+      <div class="grid grid-cols-2 gap-10 pt-10">
+        <div class="">
+          <h5 class="text-3xl font-bold">IDeyPay Ghana</h5>
+          <small class="">Fintech</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sapiente voluptates veritatis pariatur dignissimos, tempore ea. Quis
+            cum ipsa debitis repellat eos deserunt dolorum ut aspernatur
+            aperiam, porro voluptas odio!. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nihil suscipit repellendus quae sit
+            amet eaque consequuntur sint doloremque in nam magni eum
+            necessitatibus aliquam minima quis, reprehenderit aspernatur!
+            Provident, at.
+          </p>
+          <div class="flex items-center pt-2 cursor-pointer">
+            <p class="text-xl pr-2 border-b border-gray-700">Send me there</p>
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h5 class="text-3xl font-bold">Wonchuni</h5>
+          <small class="">Software Development</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sapiente voluptates veritatis pariatur dignissimos, tempore ea. Quis
+            cum ipsa debitis repellat eos deserunt dolorum ut aspernatur
+            aperiam, porro voluptas odio!. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nihil suscipit repellendus quae sit
+            amet eaque consequuntur sint doloremque in nam magni eum
+            necessitatibus aliquam minima quis, reprehenderit aspernatur!
+            Provident, at.
+          </p>
+          <div class="flex items-center pt-2 cursor-pointer">
+            <p class="text-xl pr-2 border-b border-gray-700">Send me there</p>
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h5 class="text-3xl font-bold">The Address Ghana</h5>
+          <small class="">Creative Agency</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sapiente voluptates veritatis pariatur dignissimos, tempore ea. Quis
+            cum ipsa debitis repellat eos deserunt dolorum ut aspernatur
+            aperiam, porro voluptas odio!. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nihil suscipit repellendus quae sit
+            amet eaque consequuntur sint doloremque in nam magni eum
+            necessitatibus aliquam minima quis, reprehenderit aspernatur!
+            Provident, at.
+          </p>
+          <div class="flex items-center pt-2 cursor-pointer">
+            <p class="text-xl pr-2 border-b border-gray-700">Send me there</p>
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="px-16 pt-14">
+      <h4 class="font-bold text-7xl border-b border-gray-900 pb-4">
+        Projects I've worked on
+      </h4>
+      <div class="grid grid-cols-2 gap-10 pt-10">
+        <div class="">
+          <h5 class="text-3xl font-bold">Kutana Africa</h5>
+          <small class="">VueJs • MongoDB • Nodejs</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sapiente voluptates veritatis pariatur dignissimos, tempore ea. Quis
+            cum ipsa debitis repellat eos deserunt dolorum ut aspernatur
+            aperiam, porro voluptas odio!. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nihil suscipit repellendus quae sit
+            amet eaque consequuntur sint doloremque in nam magni eum
+            necessitatibus aliquam minima quis, reprehenderit aspernatur!
+            Provident, at.
+          </p>
+          <div class="flex items-center pt-2 cursor-pointer">
+            <p class="text-xl pr-2 border-b border-gray-700">Take a look</p>
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h5 class="text-3xl font-bold">IdeyPay Till Engine</h5>
+          <small class="">VueJs • MongoDB • NodeJs</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sapiente voluptates veritatis pariatur dignissimos, tempore ea. Quis
+            cum ipsa debitis repellat eos deserunt dolorum ut aspernatur
+            aperiam, porro voluptas odio!. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nihil suscipit repellendus quae sit
+            amet eaque consequuntur sint doloremque in nam magni eum
+            necessitatibus aliquam minima quis, reprehenderit aspernatur!
+            Provident, at.
+          </p>
+          <div class="flex items-center pt-2 cursor-pointer">
+            <p class="text-xl pr-2 border-b border-gray-700">Take a look</p>
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h5 class="text-3xl font-bold">Quevy</h5>
+          <small class="">Dart • Flutter • Firebase</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sapiente voluptates veritatis pariatur dignissimos, tempore ea. Quis
+            cum ipsa debitis repellat eos deserunt dolorum ut aspernatur
+            aperiam, porro voluptas odio!. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nihil suscipit repellendus quae sit
+            amet eaque consequuntur sint doloremque in nam magni eum
+            necessitatibus aliquam minima quis, reprehenderit aspernatur!
+            Provident, at.
+          </p>
+          <div class="flex items-center pt-2 cursor-pointer">
+            <p class="text-xl pr-2 border-b border-gray-700">Take a look</p>
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h5 class="text-3xl font-bold">The Address Website</h5>
+          <small class="">VueJs</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sapiente voluptates veritatis pariatur dignissimos, tempore ea. Quis
+            cum ipsa debitis repellat eos deserunt dolorum ut aspernatur
+            aperiam, porro voluptas odio!. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nihil suscipit repellendus quae sit
+            amet eaque consequuntur sint doloremque in nam magni eum
+            necessitatibus aliquam minima quis, reprehenderit aspernatur!
+            Provident, at.
+          </p>
+          <div class="flex items-center pt-2 cursor-pointer">
+            <p class="text-xl pr-2 border-b border-gray-700">Take a look</p>
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h4 class="font-bold text-4xl flex justify-center mt-20">Want to see me ?</h4>
+    <div class="flex p-14 justify-center items-center">
+      <div class="relative">
+        <img class="w-72 h-72 object-cover rounded" src="../assets/images/klah.jpg" alt="">
+        <div class="bg-blue-100 rounded-full w-40 absolute top-2"> </div>
+      </div>
+      <div class="pl-20 ">
+        <h5 class="font-bold text-xl">A little more👌</h5>
+        <p class="pt-5 w-64">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas voluptate reiciendis quaerat quibusdam dicta aliquam facere cumque praesentium nobis. Saepe earum quia dolorem? Quod eius a soluta nemo consequuntur! Quos.</p>
+        <p class="text-xs font-semibold text-gray-400 mt-5">Nunya Yao Klah</p>
+        <p class="text-xs font-semibold text-gray-400">nunyaklah@gmail.com</p>
+      </div>
+      <div>
+        <img class="w-24" src="../assets/images/feather.png" alt="">
+      </div>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        body: ['Nunito'],
-        head: ['Poppins']
+        body: ['DM Sans'],
       }
     },
   },
